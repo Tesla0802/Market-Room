a21706e1f280d36f1811cdd4f81ff2eb1a7b297e
Nika <99128017+Tesla0802@users.noreply.github.com>
2022-12-17 18:14:46 +0400

    card

---
+++ firebase.js
@@ -0,0 +1,73 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCD6xsP_v3hSSYC5DU7e4zUAxkyOxOp2lc",
+    authDomain: "market-room-29d2c.firebaseapp.com",
+    databaseURL: "https://market-room-29d2c-default-rtdb.firebaseio.com",
+    projectId: "market-room-29d2c",
+    storageBucket: "market-room-29d2c.appspot.com",
+    messagingSenderId: "908530588479",
+    appId: "1:908530588479:web:b1a81cbec193a60c69d18a",
+    measurementId: "G-8YX3P3WEFB"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  
+  function randomID() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+      let r = (Math.random() * 16) | 0;
+      let v = c == "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
+  
+  function generateFirebaseItem(ID, value) {
+    return {
+      userid: ID,
+      data: value,
+    };
+  }
+  
+  function addElementInFirebase(REF, data) {
+    firebase
+      .database()
+      .ref(REF + randomID())
+      .set(data);
+  }
+  
+  function getArrayFromFirebase(REF) {
+    let tempArray = [];
+    firebase
+      .database()
+      .ref(REF)
+      .on("value", (response) => {
+        response.forEach((element) => {
+          tempArray.push(generateFirebaseItem(element.key, element.val()));
+        });
+      });
+    return tempArray;
+  }
+  
+  function removeRefFromFirebase(REF) {
+    firebase.database().ref(`${REF}`).remove();
+  }
+  
+  function removeElementFromFirebase(REF, id) {
+    firebase.database().ref(`${REF}/${id}`).remove();
+  }
+  
+  function getElementFromFirebaseByID(REF, id) {
+    const tempArray = getArrayFromFirebase(REF);
+    let temp = {};
+    tempArray.forEach((element) => {
+      if (element.userid === id) {
+        temp = element;
+      }
+    });
+    return temp;
+  }
+  
+  function changeDataOnFirebaseByID(REF, ID, data) {
+    firebase
+      .database()
+      .ref(REF + "/" + ID)
+      .set(data);
+  }
