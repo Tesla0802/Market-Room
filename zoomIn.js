3394e0cf035370b63342cd8c414686eb5efaa50b
Nika <99128017+Tesla0802@users.noreply.github.com>
2023-03-01 20:47:37 +0400

    products

---
+++ zoomIn.js
@@ -0,0 +1,9 @@
+var options = {
+    width: 400,
+    height: 250,
+    zoomWidth: 500,
+    offset: {vertical: 0, horizontal: 10},
+    scale: 1.5
+}
+
+new ImageZoom(document.querySelector(".card-img"), options)
